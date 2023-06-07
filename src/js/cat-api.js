3949fc8366074144f4866f91851d97d3083288ee
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-06-07 14:01:26 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,14 +1,15 @@
 
 const API_KEY = "live_lxn9abov0vj4J5dtCXBrWLbM7jh1qwtgy1kx1743hz5EqD4PvkKIW5gej7h4ry6x";
+const BASE_URL = "https://api.thecatapi.com/v1/";
 
 function getBreeds(){
-    const url = `https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`
+    const url = `${BASE_URL}breeds?api_key=${API_KEY}`
     return fetch(url)
     .then (res => res.json())
  }
 
  function fetchCatByBreed(breedId){
-  const url = `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+  const url = `${BASE_URL}images/search?api_key=${API_KEY}&breed_ids=${breedId}`
   return fetch(url)
   .then (res => res.json())
 }
